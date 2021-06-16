26c80e340b3abd2373878efa0a815e7d7b3aada5
andrewvcode <andrew.vmail7@gmail.com>
2021-06-16 22:42:43 +0300

    micro_mq

--- fascade-service/fascade.py
+++ fascade-service/fascade.py
@@ -2,7 +2,7 @@ import uuid
 import requests
 from flask import Flask, request, redirect
 import random
-
+import pika
 app = Flask(__name__)
 
 
@@ -12,20 +12,29 @@ def redirect_root():
 
 @app.route('/fascad_service', methods=['GET', 'POST'])
 def fascade():
+    micro_msg_ports = [5003,5004]
     micro_logs_ports = [5005,5006,5007]
     ml_port = random.choice(micro_logs_ports)
+    credentials = pika.PlainCredentials('rabbitmq', 'rabbitmq')
+    conn_params = pika.ConnectionParameters('localhost',5672,'/',credentials)
+    connection = pika.BlockingConnection(conn_params)
+    channel = connection.channel()
+    channel.queue_declare(queue='lab6')
 
     if request.method == 'POST':
         if request.json.get('msg'):
             content = {str(uuid.uuid4()): request.json.get('msg')}
             requests.post(f'http://localhost:{ml_port}/log', json=content)
+            channel.basic_publish(exchange='', routing_key='lab6', body=str(request.json.get('msg')))
             return 'Request accepted'
         else:
             return 'Bad Request', 400
     else:  
         getlogs = requests.get(f'http://localhost:{ml_port}/log')
-        getmessages = requests.get('http://localhost:5003/')
-        answer = '<div>' + '<h3>Logs:</h3>' + getlogs.content.decode("utf-8") + '</div>' + '<div>' + '<h3>Messages:</h3>' + getmessages.content.decode("utf-8") + '</div>'
+        getmessages = ''
+        for p in micro_msg_ports:
+            getmessages += requests.get(f'http://localhost:{p}/').content.decode("utf-8") + '</br>'
+        answer = '<div>' + '<h3>Logs:</h3>' + getlogs.content.decode("utf-8") + '</div>' + '<div>' + '<h3>Messages:</h3>' + getmessages + '</div>'
         return answer, 200   
 
 if __name__ == '__main__':
--- messages-service/msg-srv.py
+++ messages-service/msg-srv.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask,request
+import pika
+import sys
+from threading import Thread
+from time import sleep
+import sys
 
 app = Flask(__name__)
+messages_list = []
+def main():
+    credentials = pika.PlainCredentials('rabbitmq', 'rabbitmq')
+    conn_params = pika.ConnectionParameters('localhost',5672,'/',credentials)
+    connection = pika.BlockingConnection(conn_params)
+    channel = connection.channel()
 
+    channel.queue_declare(queue='lab6')
+    
+    def callback(ch, method, properties, body):    
+        print(" [x] Received %r" % body.decode())
+        messages_list.append(body.decode())
+        print(messages_list)
+    channel.basic_consume(queue='lab6', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
 
 @app.route('/')
 def msg():
-    return "Empty"
+    
+    if request.method == 'GET':
+        print(messages_list)
+        return '</br>'.join(messages_list)
 
 if __name__ == '__main__':
-    app.run(port=5003, debug=True)
+    Thread(target=main, daemon=True).start()
+    app.run(port=int(sys.argv[1]), debug=True, use_reloader=False)
